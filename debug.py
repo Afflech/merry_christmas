33b6275ca47e2c0f8f9e11516c993e4beadf3bcf
Afflech <anhquangtran1206@gmail.com>
2025-12-25 23:04:26 +0700

    Delete debug.py

--- debug.py
+++
@@ -1,9 +0,0 @@
-try:
-    import mediapipe.python.solutions
-    print("Import thành công (lạ nhỉ?)")
-except ImportError as e:
-    print("LỖI THẬT SỰ LÀ ĐÂY:")
-    print(e)
-except Exception as e:
-    print("Lỗi khác:")
-    print(e)
